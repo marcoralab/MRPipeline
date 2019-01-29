52d8b5c3f3c2c9aff7eb0ab6a0fd9d045cd37f39
Shea Andrews <s.j.f.andrews@gmail.com>
2019-01-29 10:27:24 -0500

    updated mrpresso

--- 3_Scripts/MRPRESSO_wo_outliers.R
+++ 3_Scripts/MRPRESSO_wo_outliers.R
@@ -6,7 +6,7 @@ args = commandArgs(trailingOnly = TRUE) # Set arguments from the command line
 infile = args[1] # Exposure summary statistics
 out = args[2]
 
-#infile = "~/Dropbox/Research/PostDoc-MSSM/2_MR/2_DerivedData/audit/ggtSSimp/audit_5e-6_ggtSSimp_mrpresso_MRdat.csv"
+#infile = "~/Dropbox/Research/PostDoc-MSSM/2_MR/2_DerivedData/smki/load/smki_5e-8_load_mrpresso_MRdat.csv"
 #infile = "~/Dropbox/Research/PostDoc-MSSM/2_MR/2_DerivedData/cpd/load/cpd_5e-6_load_mrpresso_MRdat.csv"
 #out = "/Users/sheaandrews/Dropbox/Research/PostDoc-MSSM/2_MR/2_DerivedData/mvpa/load/mvpa_5e-6_load"
 
@@ -44,12 +44,15 @@ if(nrow(mrdat) < nrow(mrdat.raw)){
     
     ### ===== FORMAT DATA ===== ###
     ## extract RSSobs and Pvalue 
+    message("\n Extracting RSSobs and Pvalue \n")
     
     mrpresso.p <- mrpresso.out$`MR-PRESSO results`$`Global Test`$Pvalue
     RSSobs <- mrpresso.out$`MR-PRESSO results`$`Global Test`$RSSobs
     
     
     # Write n outliers, RSSobs and Pvalue to tibble
+    message("\n Writing Results \n")
+    
     mrpresso.dat <- tibble(id.exposure = as.character(mrdat[1,'id.exposure']), 
                            id.outcome = as.character(mrdat[1,'id.outcome']), 
                            outcome = as.character(mrdat[1,'outcome']), 
