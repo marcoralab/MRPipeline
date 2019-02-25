51ebb1e8fd0c89825a30ae0880575bb21fb4ca1f
Shea Andrews <s.j.f.andrews@gmail.com>
2019-02-25 09:49:25 -0500

    testing TwoSampleMR

--- 3_Scripts/DataHarmonization.R
+++ 3_Scripts/DataHarmonization.R
@@ -9,6 +9,22 @@ outcome.code = args[4]
 proxy.snps = args[5]
 out.harmonized = args[6] # SPECIFY THE OUTPUT FILE
 
+a <- suppressWarnings(try(readLines("https://raw.githubusercontent.com/MRCIEU/TwoSampleMR/master/DESCRIPTION"), silent=TRUE))
+
+if(!class(a) == 'try-error')
+{
+  latest <- gsub("Version: ", "", a[grep("Version", a)])
+  current = utils::packageDescription('TwoSampleMR')
+
+  test <- utils::compareVersion(latest, current$Version)
+  if(test == 1)
+  {
+    packageStartupMessage("\nWarning:\nYou are running an old version of the TwoSampleMR package.\n",
+      "This version:   ", current$Version, "\n",
+      "Latest version: ", latest, "\n",
+      "Please consider updating using devtools::install_github('MRCIEU/TwoSampleMR')")
+  }
+}
 
 ### ===== Load packages ===== ###
 message("Loading packages  \n")
